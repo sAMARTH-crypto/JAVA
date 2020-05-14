5a2dd603b241a17b68b23e020903b1f88a73660d
sAMARTH-crypto <51755501+sAMARTH-crypto@users.noreply.github.com>
2020-05-14 17:37:24 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,18 @@
+//STRING IS BASICALLY A SEQUENCE OF CHARACTERS
+//always write string in double quotes
+
+
+public class string 
+{
+	public static void main(String[] args)
+	{
+		int var1 = 10;       
+		//String var2 = "20";     //integer value will be typecasted as string
+		
+		String var0 = new String("hello");      //proper way to write a string
+		
+		//System.out.println(var1+var2); //string concatenation
+	
+	}
+
+}
