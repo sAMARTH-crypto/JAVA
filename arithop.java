2d195eb1038e448695b1451d5cf0634bcff009b4
sAMARTH-crypto <51755501+sAMARTH-crypto@users.noreply.github.com>
2020-05-20 15:58:49 +0530

    Create arithop.java

---
+++ arithop.java
@@ -0,0 +1,19 @@
+//DECISION MAKING WITH OPERATORS
+public class arithop 
+{
+	public static void main (String[] args)
+	{	
+		int x = 4;
+		int y = 5;
+		if (x<y)
+		{
+			System.out.println("True");
+		}
+		else 
+		{
+				System.out.println("False");
+		}
+
+		
+	}
+
