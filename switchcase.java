b8c8201318c3256d721dced04f7a9fa55acae3cd
sAMARTH-crypto <51755501+sAMARTH-crypto@users.noreply.github.com>
2020-05-20 21:39:25 +0530

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,30 @@
+ 
+public class switchcase 
+{
+	public static void main(String[] args)
+	{
+		// switch case allows us to tackle messy if-else blocks
+		 
+		int x = 2;
+		
+		switch(x)
+		{
+		default: System.out.println("NONE");
+				break;
+		
+		case 1:
+				System.out.println("1");
+				break;
+		case 2:
+				System.out.println("2");
+				break;
+		case 3:
+				System.out.println("3");
+				break;
+		}
+		 
+		 //order of the cases can be random also 
+		
+	}
+
+}
