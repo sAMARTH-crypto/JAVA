c5fabcb6d40470a7802cb674ec8cc1e2b138d27e
sAMARTH-crypto <51755501+sAMARTH-crypto@users.noreply.github.com>
2021-01-25 12:44:09 +0530

    Create OnePlus.java

---
+++ OnePlus.java
@@ -0,0 +1,18 @@
+package phone;
+
+public class OnePlus implements Phone 
+{
+	public String Processor()
+	{
+		return "SD835";
+	}
+	public String OS()
+	{
+		return "IOS";
+	}
+	public int spaceInGB()
+	{
+		return 64;
+	}
+	
+}
